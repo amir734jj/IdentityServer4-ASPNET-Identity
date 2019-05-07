d29d43b91bf6dd5166ee1e29360d27d3fc191d9d
Unknown <seyedhesamian@northwesternmutual.com>
2019-05-06 22:37:52 -0500

    Minor

--- Api/Controllers/HomeController.cs
+++ Api/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace Api.Controllers
 {
@@ -7,11 +8,25 @@ namespace Api.Controllers
     [Route("")]
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        
         [HttpGet]
         [Route("")]
         public async Task<IActionResult> Index()
         {
             return View("Index");
+        }
+        
+        [HttpGet]
+        [Route("Info")]
+        public async Task<IActionResult> EnvironmentInfo()
+        {
+            return Ok(_environment.EnvironmentName);
         }
     }
 }
