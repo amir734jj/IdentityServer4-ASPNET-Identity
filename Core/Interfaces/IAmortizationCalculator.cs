d6adeeac47db163f2178eeacdc344e517dddb69a
hooman777 <eng.hesamyan@gmail.com>
2019-05-28 16:47:17 +0400

    Removing Core and Core.Tests

--- Core/Interfaces/IAmortizationCalculator.cs
+++
@@ -1,9 +0,0 @@
-using Core.Models;
-
-namespace Core.Interfaces
-{
-    public interface IAmortizationCalculator
-    {
-        AmortizationCalculatorResult AmortizationCalculatorFunc(decimal startingBalance, decimal apr, int months);
-    }
-}
